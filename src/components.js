11eced560ffbc7b21663eb6f3262d4dd7ea041b7
swamy.alok@gmail.com <swamy.alok@gmail.com>
2018-12-27 18:27:21 -0500

    making webpack watch files + standardizing styling our components

--- src/components.js
+++ src/components.js
@@ -4,13 +4,50 @@ class SqlQueryTab extends LitElement {
 
     constructor() {
         super();
+
+        this._onTextAreaFocus(4, 1);
     }
 
     static get properties() {
-        return { }
+        return {
+            visible: { type: Boolean },
+            editorTextAreaSize: { type: Number },
+            resultsTextAreaSize: { type: Number }
+        }
+    }
+
+    static get EDITOR_TEXT_AREA_CLASS_NAME() {
+        return 'sqlQueryTabEditor';
+    }
+
+    static get RESULTS_TEXT_AREA_CLASS_NAME() {
+        return 'sqlQueryTabResults';
     }
 
     render() {
+
+        if(!this.visible) {
+            return;
+        }
+
+        return html`
+            
+            ${this.htmlStyle}
+
+            <div id="mainContent-selectedTab">
+                <div class="sqlQueryTabMenu"></div>
+                <textarea style="flex-grow:${this.editorTextAreaSize}" @click="${this._onTextAreaFocus.bind(this, 4, 1)}" class="${(SqlQueryTab.EDITOR_TEXT_AREA_CLASS_NAME)}"></textarea>
+                <textarea style="flex-grow:${this.resultsTextAreaSize}" @click="${this._onTextAreaFocus.bind(this, 1, 4)}" readonly class="${SqlQueryTab.RESULTS_TEXT_AREA_CLASS_NAME}"></textarea>
+            </div>
+        `;
+    }
+
+    _onTextAreaFocus(editorSize, resultsSize) {
+        this.editorTextAreaSize = editorSize;
+        this.resultsTextAreaSize = resultsSize;
+    }
+
+    get htmlStyle() {
         return html`
             <style>
             .sqlQueryTabMenu {
@@ -19,38 +56,30 @@ class SqlQueryTab extends LitElement {
                 border-color: #dbdbdb;
                 border-style: solid;
                 border-width: 1px 1px 0px 1px;
-              }
-              .sqlQueryTabEditor:focus {
+            }
+            .sqlQueryTabEditor:focus, .sqlQueryTabResults:focus {
                 outline: none !important;
-              }
-              .sqlQueryTabEditor {
+            }
+            .sqlQueryTabEditor, .sqlQueryTabResults {
                 font-family: monospace;
                 border: 1px solid #dbdbdb;
                 resize: none;
                 width: 100%;
                 padding: 0.75rem;
-                flex-grow: 1;
-              }
-              .sqlQueryTabResults {
-                height: 90px;
+            }
+            .sqlQueryTabEditor {
+                //flex-grow: 4;
+            }
+            .sqlQueryTabResults {
+                //flex-grow: 1;
                 margin-top: 10px;
-                border: 1px solid #dbdbdb;
-                padding: 0.75rem;
-                cursor: text;
-              }
-              #mainContent-selectedTab {
+            }
+            #mainContent-selectedTab {
                 flex-grow: 1;
                 display: flex;
                 flex-direction: column;
-              }
-            </style>
-
-            <div id="mainContent-selectedTab">
-                <div class="sqlQueryTabMenu"></div>
-                <textarea class="sqlQueryTabEditor"></textarea>
-                <div class="sqlQueryTabResults"></div>
-            </div>
-        `;
+            }
+            </style>`;
     }
 }
 
--- webpack.conf.js
+++ webpack.conf.js
@@ -5,5 +5,6 @@ module.exports = {
   output : {
     filename :  'components.js',
   },
-  mode : 'development'
+  mode : 'development',
+  watch: true
 };
